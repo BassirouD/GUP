04cfb26905238805064658234ddbfbda2567dcfd
BassirouD <moubass.diallo@gmail.com>
2021-07-24 17:42:15 +0200

    Update du 24/07/2021

--- gup/src/app/app.module.ts
+++ gup/src/app/app.module.ts
@@ -7,11 +7,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatFormFieldModule,
+      MatMenuModule,
+      MatIconModule,
+      MatDialogModule,
+      MatInputModule,
+      ReactiveFormsModule,
+      MatSelectModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- gup/src/app/dashbord/profile/profile.page.ts
+++ gup/src/app/dashbord/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-profile',
@@ -9,7 +10,7 @@ export class ProfilePage implements OnInit {
 
     showLocationDetail = false;
 
-    constructor() {
+    constructor(private router:Router) {
     }
 
     ngOnInit() {
@@ -24,6 +25,20 @@ export class ProfilePage implements OnInit {
         setTimeout(() => {
             event.target.complete();
         }, 2000);
+    }
+
+    logOut(){
+        localStorage.removeItem('annee');
+        localStorage.removeItem('periode');
+        this.router.navigateByUrl('/');
+    }
+
+    goToChangePassword(){
+        this.router.navigateByUrl('/change-password');
+    }
+
+    goToChangeProfil(){
+        this.router.navigateByUrl('/change-profil');
     }
 
 }
